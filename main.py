a466572a311d6345ed3b9c0516598bca58b1bab6
pscrapy <nicolipaoloo90@gmail.com>
2026-02-20 19:29:53 +0100

    use settings module for main.py values

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import uvicorn
 from toy_server.app import app
+from config.settings import settings
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    uvicorn.run(app, host=settings.server_host, port=settings.server_port)
