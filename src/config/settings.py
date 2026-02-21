0e169038276c981dd0381835eba34e4a6724ce04
pscrapy <nicolipaoloo90@gmail.com>
2026-02-21 15:55:54 +0100

    added concurrency settings

--- src/config/settings.py
+++ src/config/settings.py
@@ -9,6 +9,16 @@ class ToyServerSettings(BaseSettings):
     server_port: int = Field("SERVER_PORT")
     server_host: str = Field("SERVER_HOST")
 
+    uvicorn_workers: int = Field("UVICORN_WORKERS")
+    uvicorn_concurrency_limit: int = Field("UVICORN_CONCURRENCY_LIMIT")
+    uvicorn_backlog: int = Field("UVICORN_BACKLOG")
+
+    fastapi_workers: int = Field("FASTAPI_WORKERS")
+
+    gunicorn_workers: int = Field("GUNICORN_WORKERS")
+    gunicorn_worker_connections: int = Field("GUNICORN_WORKER_CONNECTIONS")
+    gunicorn_backlog: int = Field("GUNICORN_BACKLOG")
+
     model_config = SettingsConfigDict(env_file='config.env')
 
 settings = ToyServerSettings()
