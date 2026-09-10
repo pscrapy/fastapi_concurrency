ecf3cb88615a9bf5dbfdc099edc15ab3765efba4
pscrapy <nicolipaolo90@gmail.com>
2026-09-10 22:41:17 +0200

    added seppuku router

--- src/toy_server/app.py
+++ src/toy_server/app.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 from toy_server.api.health_router import router as health_router
 from toy_server.api.sieve_router import router as sieve_router
+from toy_server.api.seppuku_router import router as seppuku_router
 from toy_server.api.settings_router import router as settings_router
 
 app = FastAPI()
@@ -9,3 +10,4 @@ app = FastAPI()
 app.include_router(health_router)
 app.include_router(sieve_router)
 app.include_router(settings_router)
+app.include_router(seppuku_router)
