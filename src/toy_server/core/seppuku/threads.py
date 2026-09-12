c2d1ed7273dc3fc1c8634da05432512a598ef2c3
pscrapy <nicolipaoloo90@gmail.com>
2026-09-12 16:14:58 +0200

    added sleepy threads and enums

--- src/toy_server/core/seppuku/threads.py
+++ src/toy_server/core/seppuku/threads.py
@@ -1,4 +1,23 @@
+import time
+from enum import StrEnum
+
+
+class TaskType(StrEnum):
+    SLEEPY = "sleepy"
+    BUSY = "busy"
+
+
+class ThreadType(StrEnum):
+    BACKGROUND = "background"
+    THREAD = "thread"
+
+
 def busywork():
     x = 1
     while True:
         x *= -1
+
+
+def sleepywork():
+    while True:
+        time.sleep(10)
